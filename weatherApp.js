e562aec52ff6f60f014668a46055f03a1b83d855
Fatima Alattas <AlattasFatima22@Gmail.com>
2024-01-09 19:17:42 +0300

    Update weatherApp.js

--- weatherApp.js
+++ weatherApp.js
@@ -1,16 +1,34 @@
+const search = document.querySelector('.search input');
+const btn = document.querySelector('.search button');
+const icon = document.querySelector(".icon");
+
 const apiKey = "790537514169f7621c8db46f98f30066";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&q=london";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-async function checkWeather(){
-    const response = await fetch(apiUrl + `appid=${apiKey}`);
-    const data = await response.json();
+async function checkWeather(city){
+
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     
-    console.log(data);
-    document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = data.main.temp;
-    document.querySelector(".humidity").innerHTML = data.main.humidity;
-    document.querySelector(".wind").innerHTML = data.wind.speed;
+    if(response.status == 404){
+        document.querySelector('.error').style.display = "block"
+        document.querySelector(".weather").style.display = "none";
+    }
+    else{
 
-}
+        const data = await response.json();
+        
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + " %";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+        
+               
+        console.log('else');
+    document.querySelector(".weather").style.display = "block";
+}//else
 
-checkWeather();
+}//function
+
+btn.addEventListener("click", ()=>{
+    checkWeather(search.value);
+});
