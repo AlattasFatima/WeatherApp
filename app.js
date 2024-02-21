cdc8630047153a4f07d2d9aefa378b5e8f57d80e
Fatima Alattas <AlattasFatima22@Gmail.com>
2024-02-21 18:24:32 +0300

    Update app.js
    Adding js function

--- app.js
+++ app.js
@@ -2,12 +2,24 @@ const express = require('express')
 const app = express()
 
 app.get('/',(req,res)=>{
+     console.log("HTML")
     res.sendFile(__dirname + '/index.html')
 })
 
-app.get('/style.css', function(req,res){
-    res.sendFile(__dirname + '/style.css')
+app.get('/weatherApp.css', function(req, res){
+    console.log(" CSS")
+    res.sendFile(__dirname + '/' + 'weatherApp.css')
 })
+
+app.get('/weatherApp.js', function(req, res){
+    console.log(" JAVA SCRIPT")
+    res.sendFile(__dirname + '/' + 'weatherApp.js')
+})
+
+// app.get('/Media', function(req, res){
+//     console.log(" Media")
+//     res.sendFile(__dirname + '/' + 'Media')
+// })
 
 app.listen(3000, ()=>{
     console.log("Server port : 3000")
