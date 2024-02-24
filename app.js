543f639be3e088c3b6d7acfef5e45266ca6b58f2
Fatima Alattas <AlattasFatima22@Gmail.com>
2024-02-24 16:12:11 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-app.use(express.static('public'))
+app.use(express.static("public"))
 
-app.listen(2222, ()=>{
-    console.log("Server port : 2222")
+app.listen(process.env.PORT||3000, ()=>{
+    console.log("Server port : 3000")
 })
