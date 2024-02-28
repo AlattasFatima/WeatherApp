b6da641535cdb8b8191481ba1471bf48a75c6280
Fatima Alattas <AlattasFatima22@Gmail.com>
2024-02-28 05:23:28 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT||3000;
+
 app.use(express.static("public"))
 
-app.listen(process.env.PORT||3000, ()=>{
-    console.log("Server port : 3000")
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: false }));
+
+app.get('/api/cities', (req, res) =>{
+    res.status(200).json({
+        citiesInfo:[
+            {
+                city: 'mecca',
+                region: 'makkah'
+            },
+            {
+                city: 'jeddah',
+                region: 'makkah'
+            },
+        ]
+    })
+})
+
+app.post('/api')
+
+app.listen(port, ()=>{
+    console.log("Server port :"+port)
 })
