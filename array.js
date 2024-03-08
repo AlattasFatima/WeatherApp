8707ce3cf9d15896dc7403ae4323077b09b9dea7
Fatima Alattas <AlattasFatima22@Gmail.com>
2024-03-08 21:36:53 +0300

    Add files via upload

---
+++ array.js
@@ -0,0 +1,12 @@
+const cities = [
+    {id: 1, name:'Mecca'},
+    {id: 2, name: 'Riyadh'},
+    {id: 3, name: 'Dammam'}
+]
+
+cities.forEach(function(elem){
+    //console.log(elem.name)
+    process.stdout.write(elem.name + "  ")
+})
+
+console.log(cities.length)
