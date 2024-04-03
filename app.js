8407c2e5281fc5b4b54fc13331c3bc8dde0b28d3
Fatima Alattas <AlattasFatima22@Gmail.com>
2024-04-03 04:31:22 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ const PORT = 3000
 app.use(express.static('public'))
 
 app.get('/', function (req, res) {
-   //res.redirect('index.html');
+   res.redirect('index1.html');
 })
 
 app.get('/cities', function (req, res) {
-    res.json(['Mecca','Riyadh','Dammam', 'Jeddah'])//try change the values in the array
+    res.json(['Abha','Abqaiq', 'Bahah', 'Dammam', 'Hufuf',
+    'Jawf', 'Kharj', 'Al-Khubar', 'Qatif', 'Taif', 'Arar',
+    'Buraydah', 'Dhahran', 'Ḥail', 'Jiddah', 'Jizan', 'Khamis Mushayt',
+    'Mecca', 'Medina', 'Najran', 'Ras Tanura', 'Riyadh',
+    'Sakaka', 'Tabuk', 'Yanbu'])
 })
 
 app.listen(PORT, function () {
