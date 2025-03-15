7933a7c9809da71cc32b33336d8c70d254e929be
Fatima Alattas <AlattasFatima22@Gmail.com>
2025-03-15 20:41:40 +0300

    Update weatherApp.js

--- public/weatherApp.js
+++ public/weatherApp.js
@@ -1,9 +1,8 @@
-const search = document.querySelector('#search select');
-const btn = document.querySelector('#search button');
+const search = document.querySelector('.search input');
+const btn = document.querySelector('.search button');
 const icon = document.querySelector(".icon");
 
-const apiKey = "";
-const api = {apiKey}
+const apiKey = "790537514169f7621c8db46f98f30066";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 async function checkWeather(city){
@@ -58,6 +57,9 @@ async function checkWeather(city){
                 break;
         }//switch
         
+          
+        
+        console.log('else');
     document.querySelector(".weather").style.display = "block";
 }//else
 
@@ -66,47 +68,3 @@ async function checkWeather(city){
 btn.addEventListener("click", ()=>{
     checkWeather(search.value);
 });
-
-async function getOptions(){
-    //here we select the drop down select element
-    const select = document.querySelector('#search select')
-    const url = 'cities'
-
-    //by default when i don't set the full url
-    // it will set the prefix for the url to the same domian as the page that made the request
-    //so it will be: 'localhost:3000/cities' same as our node server
-    //so it will be dynamic what ever was the domian it will make the request to it
-    const response = await fetch('cities');
-    if(response.ok){
-        const json = await response.json();
-        console.log(json);// the result of the request
-
-        json.forEach(city => { //we used foreach because the response was an array
-
-            console.log(city);// logging the city
-
-            //creating the option element to add it to the dropdown menu
-            const option = document.createElement('option');
-
-            //here we will appned the value to the option value and InnerText that we created
-            option.value = city
-            option.innerText = city
-            select.appendChild(option)
-            //now we will add the newly created option to the serch drop down menu
-            
-        });
-
-        //after we appended the option we need to append a place holder option
-        //we can do it like this:
-        const option = document.createElement('option');
-        option.innerText = 'Select your option'// the text to be shown
-        option.selected = true// here we making it select option
-        option.disabled = true// the user can't reselect it if he selected another option
-        select.appendChild(option)
-
-        //try to add new cities to the API in express server
-    }
-
-}
-
-getOptions();
